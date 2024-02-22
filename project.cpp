09f51f02fa9ee1c301f2c5c15338f3c7fcb8e69f
Ryan Ngo <97152810+br-ead@users.noreply.github.com>
2024-02-22 00:27:35 -0800

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -68,7 +68,7 @@ vector<StateProps> convertNFAtoDFA(const vector<StateProps>& nfa) {
 
                 // Combine qA and qB into a new state
                 StateProps newState;
-                newState.state = "("+qA.state + "," + qB.state + ")";
+                newState.state = "l"+qA.state + "," + qB.state + "l";
                 newState.start = qA.start || qB.start;
                 newState.finish = qA.finish || qB.finish;
 
