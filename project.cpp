3615c3ac6983afc4898047719530f31c39200dd2
Ryan Ngo <97152810+br-ead@users.noreply.github.com>
2024-02-21 21:06:15 -0800

    Add files via upload

---
+++ project.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct StateProps {
+    string state;
+    bool start;
+    bool finish;
+    vector<string> route_a;
+    vector<string> route_b;
+};
+
+vector<StateProps> readStatesFromFile(const string& filename) {
+    vector<StateProps> states;
+    ifstream file(filename);
+    string line;
+
+    while (getline(file, line)) {
+        StateProps s;
+        istringstream iss(line);
+        string start_str, finish_str, token;
+        if (!(iss >> s.state >> start_str >> finish_str)) { 
+            break; 
+        }
+        s.start = (start_str == "true");
+        s.finish = (finish_str == "true");
+
+        // Read routes
+        while (getline(iss, token, '-')) {
+            if (token == "x") {
+                while (getline(iss, token, '-') && token != "y") {
+                    s.route_a.push_back(token);
+                }
+            } else if (token != "y") {
+                s.route_b.push_back(token);
+            }
+        }
+
+        states.push_back(s);
+    }
+
+    return states;
+}
+
+void printStates(const vector<StateProps>& states) {
+    for (const auto& state : states) {
+        cout << "State " << state.state << " is " << (state.start ? "" : "not ") << "a start state. ";
+        cout << "It is " << (state.finish ? "" : "not ") << "a finish state. When the input is a, it will route to ";
+        
+        if (state.route_a.empty()) {
+            cout << "nothing";
+        } else {
+            for (const auto& route : state.route_a) {
+                cout << route << "/";
+            }
+        }
+
+        cout << ". When the input is b, it will route to ";
+        
+        if (state.route_b.empty()) {
+            cout << "nothing";
+        } else {
+            for (const auto& route : state.route_b) {
+                cout << route << "/";
+            }
+        }
+
+        cout << endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cout << "Usage: " << argv[0] << " <filename>" << endl;
+        return 1;
+    }
+
+    string filename = argv[1];
+
+    vector<StateProps> nfa = readStatesFromFile(filename);
+    printStates(nfa);
+
+    return 0;
+}
