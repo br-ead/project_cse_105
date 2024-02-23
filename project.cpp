4fd9675ff13e09f07fb0ecb1895e466162808d65
Ryan Ngo <97152810+br-ead@users.noreply.github.com>
2024-02-23 04:29:18 -0800

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -186,7 +186,7 @@ StateProps createNewState(const string& stateName, bool isFinal) {
 }
 
 void updateTransitionTable(const string& currentState, char input, const string& nextState, vector<StateProps>& dfa) {
-    for (size_t i = 0; i < dfa.size(); ++i) {
+    for (size_t i = 0; i < nfa.size(); ++i) {
         if (dfa[i].state == currentState) {
             if (input == 'a') {
                 dfa[i].route_a.clear();
