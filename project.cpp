19ff5b3b4698e505203c82be8cae53980a8255bc
Ryan Ngo <97152810+br-ead@users.noreply.github.com>
2024-02-23 05:41:45 -0800

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -171,11 +171,13 @@ bool isStateInDFA(const string& state, const vector<StateProps>& dfa) {
 // prompt - hello I do not want states that are similar, i.e. q0/q1 and q1/0 can you make a way for me to only add values if they are 
 // not currently present?
 
-StateProps createNewState(const string& stateName, bool isFinal) {
+StateProps createNewState(const string& stateName, bool isFinal, const vector<string>& routeA, const vector<string>& routeB) {
     StateProps newState;
     newState.state = stateName;
     newState.start = false; // New states are never start states :D
     newState.finish = isFinal;
+    newState.route_a = routeA;
+    newState.route_b=routeB;
     return newState;
 }
 
@@ -240,7 +242,74 @@ void addDeathStateIfNeeded(vector<StateProps>& dfa) {
         }
     }
 }
+void convertNFAtoDFA(const vector<StateProps>& nfa) {
+    string initialState = findInitialState(nfa);
+    vector<StateProps> dfa;
+    queue<string> newStates;
+    newStates.push(initialState);
 
+    // A map to keep track of transitions for composite states
+    map<string, vector<string>> transitionA, transitionB;
+
+    while (!newStates.empty()) {
+        string currentState = newStates.front();
+        newStates.pop();
+
+        // Initialize transition vectors for the current composite state
+        vector<string> currentStateTransA, currentStateTransB;
+
+        for (char input : {'a', 'b'}) {
+            set<string> nextStateSet = computeNextState(currentState, input, nfa);
+            string nextState = convertSetToStateName(nextStateSet);
+
+            vector<string> aggregatedRouteA, aggregatedRouteB;
+            for (const string& state : nextStateSet) {
+                auto it = find_if(nfa.begin(), nfa.end(), [&](const StateProps& sp) { return sp.state == state; });
+                if (it != nfa.end()) {
+                    if (input == 'a') {
+                        aggregatedRouteA.insert(aggregatedRouteA.end(), it->route_a.begin(), it->route_a.end());
+                    } else {
+                        aggregatedRouteB.insert(aggregatedRouteB.end(), it->route_b.begin(), it->route_b.end());
+                    }
+                }
+            }
+
+            // Ensure transitions are unique
+            sort(aggregatedRouteA.begin(), aggregatedRouteA.end());
+            aggregatedRouteA.erase(unique(aggregatedRouteA.begin(), aggregatedRouteA.end()), aggregatedRouteA.end());
+            sort(aggregatedRouteB.begin(), aggregatedRouteB.end());
+            aggregatedRouteB.erase(unique(aggregatedRouteB.begin(), aggregatedRouteB.end()), aggregatedRouteB.end());
+
+            // Update currentStateTransA or currentStateTransB based on input
+            if (input == 'a') currentStateTransA = aggregatedRouteA;
+            else currentStateTransB = aggregatedRouteB;
+
+            if (!nextState.empty() && !isStateInDFA(nextState, dfa)) {
+                bool finalState = isFinalState(nextState, nfa); // Make sure isFinalState checks correctly for composite states
+                StateProps newState = createNewState(nextState, finalState, currentStateTransA, currentStateTransB);
+                dfa.push_back(newState);
+                newStates.push(nextState);
+            }
+        }
+
+        // Directly updating the DFA state with aggregated transitions
+        auto dfaIt = find_if(dfa.begin(), dfa.end(), [&](const StateProps& sp) { return sp.state == currentState; });
+        if (dfaIt != dfa.end()) {
+            dfaIt->route_a = currentStateTransA;
+            dfaIt->route_b = currentStateTransB;
+        } else {
+            // This handles the initial state specifically if it hasn't been added yet
+            bool isFinal = isFinalState(currentState, nfa);
+            StateProps initialState = createNewState(currentState, isFinal, currentStateTransA, currentStateTransB);
+            dfa.push_back(initialState);
+        }
+    }
+
+    addDeathStateIfNeeded(dfa);
+    printStates(dfa);
+}
+
+/*
 void convertNFAtoDFA(const vector<StateProps>& nfa) {
     string initialState = findInitialState(nfa);
     vector<StateProps> dfa = initializeDFA(nfa, initialState);
@@ -282,10 +351,11 @@ void convertNFAtoDFA(const vector<StateProps>& nfa) {
     }), dfa.end()); 
     addDeathStateIfNeeded(dfa);
     printStates(dfa);
-}
+} */
 // using all of my functions generate a nfatodfa function that takes in an nfa and makes a resulting dfa based off of subset construction. 
 // Also print it using my previous function
 
+    
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         cout << "Usage: " << argv[0] << " <filename>" << endl;
